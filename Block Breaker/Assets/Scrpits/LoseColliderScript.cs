5a8b0038c57b675cfe196b9705783708c21886c2
Marley Ebejer <a100939@mcast.org.mt>
2017-11-08 12:24:06 +0100

    Added my unity games

---
+++ Block Breaker/Assets/Scrpits/LoseColliderScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseColliderScript : MonoBehaviour {
+    LevelManager levelManager = new LevelManager();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Coll");
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        levelManager.LoadNewScene("Lose");
+    }
+}
