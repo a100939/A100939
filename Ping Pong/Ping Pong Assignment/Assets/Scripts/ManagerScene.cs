76aa5c3071045142883970239817c5066b66f134
Marley Ebejer <Marley Ebejer>
2017-12-04 13:54:29 +0100

    -Added Script ManagerScene -Added Object ManagerScene -Added Script to Play and Quit Text button -If Play is pressed you go to next level -If Quit is pressed game will stop -Set Background as prefab -Added Background to the GameLevel 1 -Added the paddel ball -Ball setted matirial as bouning and added rigitbody 2d to it -Created Ball Script

---
+++ Ping Pong/Ping Pong Assignment/Assets/Scripts/ManagerScene.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ManagerScene : MonoBehaviour
+{
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Cursor.visible = true;
+    }
+    public void LoadNewScene(string sceneName)// biex tarah funitu importanti li ikun PUBLIC
+    {
+        
+        SceneManager.LoadScene(sceneName);
+    }
+    //Stop Game
+    public void Quit()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+
+}
+
