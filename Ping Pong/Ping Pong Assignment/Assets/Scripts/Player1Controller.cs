a110f0164b224c36c0db59eccfc5766b2d66ee8c
Marley Ebejer <Marley Ebejer>
2017-12-05 21:24:14 +0100

    -Added Up/DownBorders -Added Player 1 Paddle and Player 2 Paddle Rigitbody2d and collider -Added Script Player Controller 1 & 2 Scripts to move Player 1 & 2 paddles

---
+++ Ping Pong/Ping Pong Assignment/Assets/Scripts/Player1Controller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Controller : MonoBehaviour {
+    // set a private so only this scprit can see
+    private Rigidbody2D player1;
+
+	// Use this for initialization
+	void Start () {
+        //set player1 to player1paddle
+        player1 = this.GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W)|| Input.GetKey(KeyCode.UpArrow))
+        {
+            player1.velocity = new Vector2(0f, 5f);
+        }
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            player1.velocity = new Vector2(0f, -5f);
+        }
+        else
+        {
+            player1.velocity = new Vector2(0f, 0f);
+        }
+	}
+}
---
+++ Ping Pong/Ping Pong Assignment/Assets/Scripts/Player2Controller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Controller : MonoBehaviour {
+
+    // set a private so only this scprit can see
+    private Rigidbody2D player2;
+
+    // Use this for initialization
+    void Start()
+    {
+        //set player2 to player2paddle
+        player2 = this.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Cursor.visible = false ;
+        float mousePosInUnits = (Input.mousePosition.x / Screen.width * 16) - 8;
+
+        Vector3 move = new Vector3(mousePosInUnits, this.transform.position.y,this.transform.position.z);
+       
+         move.y= Mathf.Clamp(mousePosInUnits,-6.5f,6.5f);
+        this.transform.position = move; 
+    }
+   
+}
